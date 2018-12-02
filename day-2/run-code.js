bf426159cec709efa0810652f6a2c44a58ed6fba
joshb <joshb@spotify.com>
2018-12-02 09:46:48 -0500

    feature(day2): add day 2

---
+++ day-2/run-code.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input', 'utf8');
+const inputCleaned = input.split('\n');
+
+
+// PT 1
+let hasTwo = 0;
+let hasThree = 0;
+inputCleaned.forEach(item => {
+  console.log(item.split(''));
+  const itemCount = item.split('').reduce((prev, curr) => {
+    if (!prev[curr]) {
+      prev[curr] = 1;
+      return prev;
+    }
+    prev[curr] = prev[curr] + 1;
+    return prev;
+  }, {});
+  const hasTwoStr = !!Object.values(itemCount).filter(i => i === 2).length;
+  const hasThreeStr = !!Object.values(itemCount).filter(i => i === 3).length;
+  if (hasThreeStr) {
+    hasThree++;
+  }
+  if (hasTwoStr) {
+    hasTwo++;
+  }
+});
+
+//PT 2
+let cleanedString;
+inputCleaned.forEach((str, index) => {
+  if (cleanedString) {return;}
+  for (x = index + 1; x < inputCleaned.length; x++) {
+    const tempStr = inputCleaned[x].split('')
+    const stringCommon = str.split('').filter((i, dIndex) => i === tempStr[dIndex]);
+    if (stringCommon.length === tempStr.length - 1) {
+      cleanedString = stringCommon;
+      break;
+    }
+  }
+})
+
+console.log('PT 1', hasThree * hasTwo);
+console.log('PT 2', cleanedString.join(''));
